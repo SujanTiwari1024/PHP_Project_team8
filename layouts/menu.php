b2a9e06e5a7aab1e6785eeb163733245427351fd
V1ltzu <villelehtonen1@gmail.com>
2023-02-16 23:17:36 +0200

    Fixed the menu images a lil bit

--- layouts/menu.php
+++ layouts/menu.php
@@ -10,14 +10,14 @@
         <h1>It's not just food, <br> It's the experience.</h1>
         <article class="maincourse">
             <div class="food_item">
-                <img src="../assets/images/Meal-Plan-plate-protein.png" alt="" width="50%">
+                <img src="../assets/images/mixveg.png" alt="" width="50%">
                 <h3>Vegetables Mix</h3>
                 <p>Lorem ipsum dolor sit amet consectetur</p>
                 <h3>6.30$</h3>
             </div>
             <div class="food_item">
-                <img src="../assets/images/Meal-Plan-plate-protein.png" alt="" width="50%">
-                <h3>Salad Dressing</h3>
+                <img src="../assets/images/salad.png" alt="" width="50%">
+                <h3>Salad</h3>
                 <p>Lorem ipsum dolor sit amet consectetur</p>
                 <h3>10.30$</h3>
             </div>
@@ -34,19 +34,19 @@
         <h1>Desserts</h1>
         <article class="maincourse">
             <div class="food_item">
-                <img src="../assets/images/Meal-Plan-plate-protein.png" alt="" width="50%">
-                <p>Full egg, loaf of meat (180gm), butter, some garlic.</p>
-                <h3>13.30$</h3>
+                <img src="../assets/images/cake.png" alt="" width="50%">
+                <p>A cake slice.</p>
+                <h3>3.30$</h3>
             </div>
             <div class="food_item">
-                <img src="../assets/images/Meal-Plan-plate-protein.png" alt="" width="50%">
-                <p>Full egg, loaf of meat (180gm), butter, some garlic.</p>
-                <h3>13.30$</h3>
+                <img src="../assets/images/cupcake.png" alt="" width="50%">
+                <p>A cupcake.</p>
+                <h3>2$</h3>
             </div>
             <div class="food_item">
-                <img src="../assets/images/Meal-Plan-plate-protein.png" alt="" width="50%">
-                <p>Full egg, loaf of meat (180gm), butter, some garlic.</p>
-                <h3>13.30$</h3>
+                <img src="../assets/images/deserts1.png" alt="" width="50%">
+                <p>Dessert tray.</p>
+                <h3>7$</h3>
             </div>
         </article>
 
