62333d9cf5c6272b3beca721b3deaf83b3bf75c6
V1ltzu <villelehtonen1@gmail.com>
2023-03-01 20:22:16 +0200

    Added a read more btn to aboutus

--- layouts/aboutus.php
+++ layouts/aboutus.php
@@ -15,9 +15,13 @@
                 <h4>
                     We welcome you to sit back, unwind and appreciate the lovely sights and hints of nature while our best gourmet expert sets you up for a scrumptious dinner utilizing the best and freshest ingredients. The Cool Restaurant’s legacy comes from our parents, built-in 1963.
                 </h4>
-                <div><p>Phone: 044 123 4567</p>
-                <p>Email: cool.restaurant@gmail.com</p>
-                <p><h5>Address: Feikkikatu 7, Hämeenlinna</h5></p> 
+                <div>
+                <P id="more" style="display:none;">
+                Phone: 044 123 4567 <br>
+                Email: cool.restaurant@gmail.com <br>
+                Address: Feikkikatu 7, Hämeenlinna
+            </P>
+            <button onclick="document.getElementById('more').style.display='block';this.style.display='none';">Contact Information</button>
             </div>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15480.144212062587!2d24.459117!3d60.988247799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x468e5d7f1a184209%3A0xe69734d5c10042bd!2sHAMK%20H%C3%A4me%20University%20of%20Applied%20Sciences!5e0!3m2!1sen!2sfi!4v1674676818463!5m2!1sen!2sfi" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
